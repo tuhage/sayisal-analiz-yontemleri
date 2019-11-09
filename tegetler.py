acfdd1006bed2d30a35cd2a5382dbd580e3488da
tuhage <tuhage@gmail.com>
2019-11-09 17:56:52 +0300

    tegetler complete

--- tegetler.py
+++ tegetler.py
@@ -1,10 +1,29 @@
 import math
 
 def y(x):
-    return x-math.cos(x)
+    return x-math.cos(x)-0.25
+def y_derivate(x):
+    return 1+math.sin(x)
+def y_2_derivate(x):
+    return math.cos(x)
+def q(x):
+    return x-(y(x)/y_derivate(x))
+
 
 def tegetler(a,b,epsilon):
-    pass
+    
+    if(y(a)*y_2_derivate(a)>0):
+        x=a
+    else:
+        x=b
+    
+    while True:
+        xi=x
+        x=q(xi)
+        if abs(xi-x)<epsilon:
+            print("x = ",x)
+            break
+    
 
 
 if __name__ == "__main__":
