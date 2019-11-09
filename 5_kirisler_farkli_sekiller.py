7d6faeca1eea83338a56d0b190420fda3abc946b
tuhage <tuhage@gmail.com>
2019-11-09 18:15:33 +0300

    6

---
+++ 5_kirisler_farkli_sekiller.py
@@ -0,0 +1,33 @@
+import math
+
+def y(x):
+    return x-math.cos(x)-0.25
+def y_derivate(x):
+    return 1+math.sin(x)
+def y_2_derivate(x):
+    return math.cos(x)
+def q(x,x0):
+    return (x0*y(x)-x*y(x0))/(y(x)-y(x0))
+
+
+def tegetler(a,b,epsilon):
+    
+    if(y(a)*y_2_derivate(a)>0):
+        x=b
+        x0=a
+    else:
+        x=a
+        x0=b
+    
+    while True:
+        xi=x
+        xo=x
+        x=q(xi,x0)
+        if abs(xi-x)<epsilon:
+            print("x = ",x)
+            break
+        x0=xo 
+
+
+if __name__ == "__main__":
+    tegetler(0,1,0.001)
