c88730d4c49a304e7a2f86de19e5f9a5c24ce3c5
tuhage <tuhage@gmail.com>
2019-11-09 17:41:47 +0300

    basic iteration complete

--- basit_iterasyon.py
+++ basit_iterasyon.py
@@ -10,9 +10,12 @@ def q_derivate(x):
     return -math.sin(x)
     
 def basit_iterasyon(a,b,epsilon):
-    
+    x=q(b)
     while True:
-        pass
-
+        xi=x
+        x=q(x)
+        if abs(xi-x)<epsilon:
+            print("x = ",x)
+            break
 if __name__ == "__main__":
     basit_iterasyon(0.5,1,0.01)
